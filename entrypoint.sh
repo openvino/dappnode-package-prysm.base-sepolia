3f037cb7e18062717300cffd0551cf84f123ac00
Mike Tango Bravo <mtb@openvino.org>
2025-05-12 09:51:13 -0300

    try beaconstate-sepolia.chainsafe.io

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,6 @@ exec /beacon-chain \
   --accept-terms-of-use \
   --rpc-host=0.0.0.0 \
   --grpc-gateway-host=0.0.0.0 \
-  --genesis-beacon-api-url=https://checkpoint-sync.sepolia.ethpandaops.io \
-  --checkpoint-sync-url=https://checkpoint-sync.sepolia.ethpandaops.io \
+  --genesis-beacon-api-url=https://beaconstate-sepolia.chainsafe.io \
+  --checkpoint-sync-url=https://beaconstate-sepolia.chainsafe.io \
   --suggested-fee-recipient=0x56B6f70462EfC55566448e3c054150BFd43aB6a2  # replace with your own...or not
